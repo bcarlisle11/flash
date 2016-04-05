e5ab5a5cbe4ea76a3f1991ca657408a9d000e086
NeedsMoreGravitas <dyshallow@gmail.com>
2016-04-04 20:33:42 -0500

    Revert "Replaced individual files with complete directory"
    This reverts commit 0c436a813ed7ff9cc0cf9a913d914495c8ddaeb1.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+<<<<<<< HEAD
 # Flash
+=======
+# flash
+
+#Dylan test commit
+>>>>>>> parent of 0c436a8... Replaced individual files with complete directory
--- inc.reservations.php
+++ inc.reservations.php
@@ -1,11 +1,41 @@
 <?php
 /**
+<<<<<<< HEAD
  * Team Flash
  * CS 321
  * inc.reservations.php
  * Used to add extra php methods to reservations.php
  */
 
+=======
+ * Created by PhpStorm.
+ * User: bcarlisle11
+ * Date: 3/29/16
+ * Time: 7:27 PM
+ */
+
+$task = $_GET['task'];
+//populateReservations();
+
+if ($task == populateReservations){
+    populateReservations();
+}
+
+function getPDO($dbname) {
+    include ('../db/flashDB.php');
+
+    try {
+        $pdo = new PDO(DB_CONNECTION_STRING . ";dbname=$dbname", DB_USER, DB_PWD);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
+
+    } catch (PDOException $e) {
+        $GLOBALS['ConfirmationMessage'] = $e->getMessage();
+    }
+}// end getPDO()
+
+>>>>>>> parent of 0c436a8... Replaced individual files with complete directory
 function dropPhp($name, array $myArr, $chosen=null)
 {
     //open select html tag
@@ -24,6 +54,33 @@ function dropPhp($name, array $myArr, $chosen=null)
     $drop .= '</select>'."\n";
 
     return $drop;
+<<<<<<< HEAD
     echo $drop;
 }
+=======
+}
+
+function populateReservations()
+{
+    try {
+
+        $pdo = getPDO('flash');
+
+        $sql = <<<EOSQL
+        /* !40000 ALTER TABLE employee DISABLE KEYS */;
+        INSERT INTO employee
+        VALUES
+            (1, 'Brendan','Carlisle');
+            /*!40000 ALTER TABLE employee ENABLE KEYS */;
+        
+EOSQL;
+
+        $pdo->exec($sql);
+        $pdo = null;
+    } catch (PDOException $e) {
+    $GLOBALS['ConfirmationMessage'] = $e->getMessage();
+}
+
+    }
+>>>>>>> parent of 0c436a8... Replaced individual files with complete directory
 ?>
