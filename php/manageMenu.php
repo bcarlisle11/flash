137b1dd38c26c3584c837d0035e69cc8f15cbc44
NeedsMoreGravitas <dyshallow@gmail.com>
2016-04-10 22:23:25 -0500

    menu pulls all info from db instead of just names

--- php/manageMenu.php
+++ php/manageMenu.php
@@ -8,6 +8,10 @@ include_once 'pageNav.php';
 
 //echo(getDateOfDay('Monday', true));
 
+//if(isset($_POST['week'])){
+//    echo($_POST['week']);
+//}
+
 function getDateOfDay($day, $full) {
 
 
@@ -98,16 +102,23 @@ function getDateOfDay($day, $full) {
         }
     }
 
+    if(isset($_POST['week'])){
+        $next = $_POST['week'];
+        //echo($_POST['week'] + "<br>");
+    } else {
+        $next = 0;
+    }
+    
+    $modifier += $next * 7;
 
 
-
-    if($full){
+    if ($full) {
         $date = date("Y-m-d", mktime(0, 0, 0, date("m"), date("d") + $modifier, date("Y")));
-    }else{
+    } else {
         $date = date("m/d", mktime(0, 0, 0, date("m"), date("d") + $modifier, date("Y")));
     }
 
-    
+
 //
 //    if (date("l") == "Saturday") {
 //        $monday = date("Y-m-d", mktime(0, 0, 0, date("m"), date("d") + $modifier, date("Y"))); // day after tomorrow
@@ -137,9 +148,9 @@ function outputTableHeader() {
     $fri = getDateOfDay("Friday", false);
 
 
-    
-    
-    
+
+
+
     echo("<tr>
 <th colspan = \"2\">Monday <br>{$mon}</th>
 <th colspan = \"2\">Tuesday <br>{$tues}</th>
@@ -151,44 +162,122 @@ function outputTableHeader() {
 
 function outputTableRows() {
 
-     try {
+    try {
         $date = getDateOfDay("Monday", true);
         $pdo = getPDO('flash');
         $sql = "SELECT `menuEntree`, `menuSide1`, `menuSide2`, `menuSpecial` FROM `menu` WHERE menuDate = '$date'";
         $queryResult = $pdo->query($sql);
         $mon = $queryResult->fetch(PDO::FETCH_ASSOC);
+
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$mon['menuEntree']}'";
+        $queryResult = $pdo->query($sql);
+        $monEntree = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$mon['menuSide1']}'";
+        $queryResult = $pdo->query($sql);
+        $monSide1 = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$mon['menuSide2']}'";
+        $queryResult = $pdo->query($sql);
+        $monSide2 = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$mon['menuSpecial']}'";
+        $queryResult = $pdo->query($sql);
+        $monSpecial = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
         
         $date = getDateOfDay("Tuesday", true);
         $sql = "SELECT `menuEntree`, `menuSide1`, `menuSide2`, `menuSpecial` FROM `menu` WHERE menuDate = '$date'";
         $queryResult = $pdo->query($sql);
         $tue = $queryResult->fetch(PDO::FETCH_ASSOC);
         
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$tue['menuEntree']}'";
+        $queryResult = $pdo->query($sql);
+        $tueEntree = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$tue['menuSide1']}'";
+        $queryResult = $pdo->query($sql);
+        $tueSide1 = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$tue['menuSide2']}'";
+        $queryResult = $pdo->query($sql);
+        $tueSide2 = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$tue['menuSpecial']}'";
+        $queryResult = $pdo->query($sql);
+        $tueSpecial = $queryResult->fetch(PDO::FETCH_ASSOC);
+
         $date = getDateOfDay("Wednesday", true);
         $sql = "SELECT `menuEntree`, `menuSide1`, `menuSide2`, `menuSpecial` FROM `menu` WHERE menuDate = '$date'";
         $queryResult = $pdo->query($sql);
         $wed = $queryResult->fetch(PDO::FETCH_ASSOC);
         
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$wed['menuEntree']}'";
+        $queryResult = $pdo->query($sql);
+        $wedEntree = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$wed['menuSide1']}'";
+        $queryResult = $pdo->query($sql);
+        $wedSide1 = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$wed['menuSide2']}'";
+        $queryResult = $pdo->query($sql);
+        $wedSide2 = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$wed['menuSpecial']}'";
+        $queryResult = $pdo->query($sql);
+        $wedSpecial = $queryResult->fetch(PDO::FETCH_ASSOC);
+
         $date = getDateOfDay("Thursday", true);
         $sql = "SELECT `menuEntree`, `menuSide1`, `menuSide2`, `menuSpecial` FROM `menu` WHERE menuDate = '$date'";
         $queryResult = $pdo->query($sql);
         $thu = $queryResult->fetch(PDO::FETCH_ASSOC);
         
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$thu['menuEntree']}'";
+        $queryResult = $pdo->query($sql);
+        $thuEntree = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$thu['menuSide1']}'";
+        $queryResult = $pdo->query($sql);
+        $thuSide1 = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$thu['menuSide2']}'";
+        $queryResult = $pdo->query($sql);
+        $thuSide2 = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$thu['menuSpecial']}'";
+        $queryResult = $pdo->query($sql);
+        $thuSpecial = $queryResult->fetch(PDO::FETCH_ASSOC);
+
         $date = getDateOfDay("Friday", true);
         $sql = "SELECT `menuEntree`, `menuSide1`, `menuSide2`, `menuSpecial` FROM `menu` WHERE menuDate = '$date'";
         $queryResult = $pdo->query($sql);
         $fri = $queryResult->fetch(PDO::FETCH_ASSOC);
-    
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$fri['menuEntree']}'";
+        $queryResult = $pdo->query($sql);
+        $friEntree = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$fri['menuSide1']}'";
+        $queryResult = $pdo->query($sql);
+        $friSide1 = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$fri['menuSide2']}'";
+        $queryResult = $pdo->query($sql);
+        $friSide2 = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
+        $sql = "SELECT `itemName`, `itemCal`, `itemPrice`, `itemDesc` FROM `items` WHERE itemName = '{$fri['menuSpecial']}'";
+        $queryResult = $pdo->query($sql);
+        $friSpecial = $queryResult->fetch(PDO::FETCH_ASSOC);
+        
     } catch (PDOException $e) {
         
     }
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+
+
+
     echo("<tr>
     <td id = \"entreeRow\" colspan = \"2\">Entree</td>
     <td id = \"entreeRow\" colspan = \"2\">Entree</td>
@@ -198,16 +287,16 @@ function outputTableRows() {
 
     </tr>
     <tr>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$mon['menuEntree']}</td>
-    <td>Cal: 400 <br> $5.00</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$tue['menuEntree']}</td>
-    <td>Cal: 400 <br> $5.00</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$wed['menuEntree']}</td>
-    <td>Cal: 400 <br> $5.00</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$thu['menuEntree']}</td>
-    <td>Cal: 400 <br> $5.00</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$fri['menuEntree']}</td>
-    <td>Cal: 400 <br> $5.00</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$monEntree['itemName']}</td>
+    <td>Cal: {$monEntree['itemCal']}<br>\$" . sprintf('%.02f', $monEntree['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$tueEntree['itemName']}</td>
+    <td>Cal: {$tueEntree['itemCal']}<br> $" . sprintf('%.02f', $tueEntree['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$wedEntree['itemName']}</td>
+    <td>Cal: {$wedEntree['itemCal']}<br> $" . sprintf('%.02f', $wedEntree['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$thuEntree['itemName']}</td>
+    <td>Cal: {$thuEntree['itemCal']}<br> $" . sprintf('%.02f', $thuEntree['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$friEntree['itemName']}</td>
+    <td>Cal: {$friEntree['itemCal']}<br> $" . sprintf('%.02f', $friEntree['itemPrice']) . "</td>
     </tr>
     <tr>
     <td id = \"sideRow\" colspan = \"2\">Sides</td>
@@ -218,28 +307,28 @@ function outputTableRows() {
 
     </tr>
     <tr>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$mon['menuSide1']}</td>
-    <td>Cal: 150 <br> $1.00</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$tue['menuSide1']}</td>
-    <td>Cal: 150 <br> $1.00</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$wed['menuSide1']}</td>
-    <td>Cal: 150 <br> $1.00</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$thu['menuSide1']}</td>
-    <td>Cal: 150 <br> $1.00</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$fri['menuSide1']}</td>
-    <td>Cal: 150 <br> $1.00</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$monSide1['itemName']}</td>
+    <td>Cal: {$monSide1['itemCal']}<br> $" . sprintf('%.02f', $monSide1['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$tueSide1['itemName']}</td>
+    <td>Cal: {$tueSide1['itemCal']}<br> $" . sprintf('%.02f', $tueSide1['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$wedSide1['itemName']}</td>
+    <td>Cal: {$wedSide1['itemCal']}<br> $" . sprintf('%.02f', $wedSide1['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$thuSide1['itemName']}</td>
+    <td>Cal: {$thuSide1['itemCal']}<br> $" . sprintf('%.02f', $thuSide1['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$friSide1['itemName']}</td>
+    <td>Cal: {$friSide1['itemCal']}<br> $" . sprintf('%.02f', $friSide1['itemPrice']) . "</td>
     </tr>
     <tr>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$mon['menuSide2']}</td>
-    <td>Cal: 150 <br> $1.00</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$tue['menuSide2']}</td>
-    <td>Cal: 150 <br> $1.00</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$wed['menuSide2']}</td>
-    <td>Cal: 150 <br> $1.00</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$thu['menuSide2']}</td>
-    <td>Cal: 150 <br> $1.00</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$fri['menuSide2']}</td>
-    <td>Cal: 150 <br> $1.00</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$monSide2['itemName']}</td>
+    <td>Cal: {$monSide2['itemCal']}<br> $" . sprintf('%.02f', $monSide2['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$tueSide2['itemName']}</td>
+    <td>Cal: {$tueSide2['itemCal']}<br> $" . sprintf('%.02f', $tueSide2['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$wedSide2['itemName']}</td>
+    <td>Cal: {$wedSide2['itemCal']}<br> $" . sprintf('%.02f', $wedSide2['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$thuSide2['itemName']}</td>
+    <td>Cal: {$thuSide2['itemCal']}<br> $" . sprintf('%.02f', $thuSide2['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$friSide2['itemName']}</td>
+    <td>Cal: {$friSide2['itemCal']}<br> $" . sprintf('%.02f', $friSide2['itemPrice']) . "</td>
     </tr>
     <tr>
     <td id = \"specialRow\" colspan = \"2\">Special</td>
@@ -250,18 +339,17 @@ function outputTableRows() {
 
     </tr>
     <tr>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$mon['menuSpecial']}</td>
-    <td>Cal: 350 <br> $3.50</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$tue['menuSpecial']}</td>
-    <td>Cal: 350 <br> $3.50</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$wed['menuSpecial']}</td>
-    <td>Cal: 350 <br> $3.50</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$thu['menuSpecial']}</td>
-    <td>Cal: 350 <br> $3.50</td>
-    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$fri['menuSpecial']}</td>
-    <td>Cal: 350 <br> $3.50</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$monSpecial['itemName']}</td>
+    <td>Cal: {$monSpecial['itemCal']} <br> $" . sprintf('%.02f', $monSpecial['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$tueSpecial['itemName']}</td>
+    <td>Cal: {$tueSpecial['itemCal']} <br> $" . sprintf('%.02f', $tueSpecial['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$wedSpecial['itemName']}</td>
+    <td>Cal: {$wedSpecial['itemCal']} <br> $" . sprintf('%.02f', $wedSpecial['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$wedSpecial['itemName']}</td>
+    <td>Cal: {$thuSpecial['itemCal']} <br> $" . sprintf('%.02f', $thuSpecial['itemPrice']) . "</td>
+    <td class = \"menuItem\" onclick = \"itemSelect(this);\">{$friSpecial['itemName']}</td>
+    <td>Cal: {$friSpecial['itemCal']} <br> $" . sprintf('%.02f', $friSpecial['itemPrice']) . "</td>
     </tr>");
-    
 }
 
 function getPDO($dbname) {
@@ -279,7 +367,7 @@ function getPDO($dbname) {
 ?>
 <script>
     function itemSelect(td) {
-        window.open("items.php/?item=" + td.innerHTML);
+        window.open("manageItems.php/?item=" + td.innerHTML);
     }
 </script>
 <!DOCTYPE html>
@@ -294,11 +382,26 @@ function getPDO($dbname) {
     <body>
         <main id="menu">
 
+            <?php
+            
+            if(isset($_POST['week'])){
+                $nextWeekValue = $_POST['week'] + 1;
+                $prevWeekValue = $_POST['week'] - 1;
+            } else{
+                $nextWeekValue = 1;
+                $prevWeekValue = -1;
+            }
+            
+            
+            echo("<div id=\"section\">
+                <form action=\"manageMenu.php\" method=\"post\">
+                    Select Week:
+                    <button type=\"submit\" name=\"week\" value=\"{$prevWeekValue}\">Previous Week</button>
+                    <button type=\"submit\" name=\"week\" value=\"{$nextWeekValue}\">Next Week</button>
+                </form>");
+            
+?>
 
-            <div id="section">
-                <button>Previous Week</button>
-                <button>Select Week</button>
-                <button>Next Week</button>
 
 
                 <table id="menu" align="center">
