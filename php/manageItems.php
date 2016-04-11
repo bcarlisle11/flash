643ad2d59e00dd728879d0a2389cfa39c34639a2
NeedsMoreGravitas <dyshallow@gmail.com>
2016-04-10 21:05:08 -0500

    began managing items

--- php/manageItems.php
+++ php/manageItems.php
@@ -1,8 +1,246 @@
+<base href="/flash/php/manageItems.php"/> <!-- hax -->
 <?php
-
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+include_once 'pageNav.php'; // change to manager facing pageNav
+$item = "";
+
+$name;
+$price;
+$cals;
+if (isset($_GET['item'])) {
+    displayItem();
+} elseif (isset($_POST['typeSelect'])) {
+//    echo($_POST['typeSelect']);
+    displayTable();
+} elseif(isset($_POST['name'])){ 
+    addItemToDb();
+}elseif(isset($_POST['addItem'])){
+    //echo($_POST['addItem']);
+    addItemForm();
+} else{
+    displayButtons();
+}
+
+function addItemToDb(){
+    
+    try {
+        $pdo = getPDO('flash');
+
+        $type = $_POST['type'];
+        $name = $_POST['name'];
+        $cals = $_POST['cals'];
+        $price = $_POST['price'];
+        $desc = $_POST['desc'];
+        
+
+        $sql = "INSERT INTO `items` (`itemName`, `itemType`, `itemPrice`, `itemCal`, `itemDesc`) VALUES ('$name', '$type', '$price', '$cals', '$desc')";
+
+        $queryResult = $pdo->query($sql);
+
+    } catch (PDOException $e) {
+        
+    }
+    
+    
+    
+    
+}
+
+
+function populateTableVars() {
+    try {
+        $pdo = getPDO('flash');
+
+        $type = $_POST['typeSelect'];
+
+        $sql = "SELECT `itemName`, `itemPrice`, `itemCal` FROM `items` WHERE itemType = '$type'";
+
+        $queryResult = $pdo->query($sql);
+
+        global $name, $price, $cals;
+
+
+
+        while ($row = $queryResult->fetch(PDO::FETCH_ASSOC)) {
+
+            $priceFormatted = sprintf('%.2f', $row['itemPrice']);
+            echo("
+                      <tr>
+                        <td class=\"listItem\" onclick=\"itemSelect(this);\">{$row['itemName']}</td>
+                        <td onclick=\"itemSelect(this);\">\${$priceFormatted}</td>
+                        <td onclick=\"itemSelect(this);\">{$row['itemCal']}</td>
+                      
+                          </tr>
+                      
+                          ");
+        }
+    } catch (PDOException $e) {
+        
+    }
+}
+
+function outputRows() {
+
+    populateTableVars();
+//    
+//    for($i = 0; $i <= count($name); $i++){
+//        echo("
+//            <tr>
+//        <td>$name[i]</td>
+//        <td>$$price[i]</td>
+//        <td>$cals[i]</td>
+//            </tr>
+//                ");
+//    }
+}
+
+function outputItem() {
+    try {
+        $pdo = getPDO('flash');
+
+        $name = $_GET['item'];
+
+        $sql = "SELECT `itemName`, `itemPrice`, `itemCal`, `itemDesc` FROM `items` WHERE itemName = '$name'";
+
+        $queryResult = $pdo->query($sql);
+
+
+
+        while ($row = $queryResult->fetch(PDO::FETCH_ASSOC)) {
+
+            $priceFormatted = sprintf('%.2f', $row['itemPrice']);
+            echo("
+                    <div id=\"item\">
+                        <h1>{$row['itemName']}</h1>
+                        <p>\${$priceFormatted}, Calories: {$row['itemCal']}</p>
+                        <p>{$row['itemDesc']}</p>
+                    </div>
+                        ");
+        }
+    } catch (PDOException $e) {
+        
+    }
+}
+
+function getPDO($dbname) {
+    include('../db/flashDB.php');
+
+    try {
+        $pdo = new PDO(DB_CONNECTION_STRING . ";dbname=$dbname", DB_USER, DB_PWD);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
+    } catch (PDOException $e) {
+        //$GLOBALS['ConfirmationMessage'] = $e->getMessage();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<script>
+    function itemSelect(td) {
+        window.open("manageItems.php/?item=" + td.innerHTML, "_self");
+    }
+</script>
+<html>
+    <head>
+
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="/flash/css/flash.css">
+        <title>Flash Food Court</title>
+    </head>
+    <body>
+
+        <main>
+
+
+
+<?php
+
+function addItemForm(){
+
+echo("<div id=\"form\" class=\"center\">
+    
+    <h1>Add an item to the database:</h1>
+    <form method=\"post\" action=\"manageItems.php\">
+        <label for=\"name\">Item name:</label> <input type=\"text\" name=\"name\" size=\"50\" value=\"\"><br><br>
+        <label for=\"cals\">Item calories:</label> <input type=\"number\" name=\"cals\" min=\"0\" step=\"1\" value=\"\"><br><br>
+        <label for=\"price\">Item price:</label> <input type=\"number\" name=\"price\" min=\"0\" step=\".01\" value=\"\"><br><br>
+        <label for=\"desc\">Item Description:</label> <textarea rows=\"4\" cols=\"50\" name=\"desc\" size=\"500\" value=\"\"></textarea><br><br>
+        <input type=\"text\" name=\"type\" value=\"{$_POST['addItem']}\" style=\"display:none;\">
+        <input type=\"submit\">
+    </form>
+    
+</div>");
+    
+    
+}
+
+function displayButtons() {
+    echo("<div id=\"itemSelect\">
+                <h1>Select Which items to view:</h1>
+                <form action=\"manageItems.php\" method=\"post\">
+                    <button type=\"submit\" name=\"typeSelect\" value=\"entree\">View Entrees</button>
+                    <button type=\"submit\" name=\"typeSelect\" value=\"special\">View Specials</button>
+                    <button type=\"submit\" name=\"typeSelect\" value=\"side\">View Sides</button>
+                    <br><br>
+                    <button type=\"submit\" name=\"addItem\" value=\"entree\">Add Entree</button>
+                    <button type=\"submit\" name=\"addItem\" value=\"special\">Add Special</button>
+                    <button type=\"submit\" name=\"addItem\" value=\"side\">Add Side</button>
+                </form>
+                ");
+}
+
+function displayTable() {
+    // output item select page
+    echo("<div id=\"itemSelect\">
+
+                <h1>Select Which items to view:</h1>
+                <form action=\"items.php\" method=\"post\">
+                    <button type=\"submit\" name=\"typeSelect\" value=\"entree\">View Entrees</button>
+                    <button type=\"submit\" name=\"typeSelect\" value=\"special\">View Specials</button>
+                    <button type=\"submit\" name=\"typeSelect\" value=\"side\">View Sides</button>
+                </form>
+                
+                <div id=\"itemTableDiv\">
+                    <table id=\"itemTable\">
+                        <colgroup>
+                            <col id=\"nameColumn\">
+                            <col id =\"priceColumn\">
+                            <col id =\"calColumn\">
+                        </colgroup>
+                        <tr>
+                            <th>Item Name</th>
+                            <th>Price</th>
+                            <th>Calories</th>
+                        </tr>
+                        <tr>");
+
+    outputRows();
+
+    echo("</tr>
+                    </table>
+
+                </div>
+                </div>
+            ");
+}
+
+function displayItem() {
+    // output item description page
+    outputItem();
+}
+?>
+        </main>
+
+        <div id="footer">
+            Team Flash
+        </div>
+    </body>
+</html>
 
