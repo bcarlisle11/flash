735e82df223869c7f5348b992e216b0b76940a1f
NeedsMoreGravitas <dyshallow@gmail.com>
2016-04-10 22:50:54 -0500

    started createMenu

---
+++ php/createMenu.php
@@ -0,0 +1,52 @@
+<?php ?>
+<div id="addMenu">
+    <div id="form" class="center">
+        Select items for the Menu<br><br>
+        <form method ="post" action="createMenu.php">
+            <label for="entree">Entree</label>
+            <select name="entree">
+                <option value="0">Select Day</option>
+                <option value="Monday">Monday</option>
+                <option value="Tuesday">Tuesday</option>
+                <option value="Wednesday">Wednesday</option>
+                <option value="Thursday">Thursday</option>
+                <option value="Friday">Friday</option>
+            </select>
+            <label for="side1">Side One:</label>
+            <select name="side1">
+                <option value="0">Select Day</option>
+                <option value="Monday">Monday</option>
+                <option value="Tuesday">Tuesday</option>
+                <option value="Wednesday">Wednesday</option>
+                <option value="Thursday">Thursday</option>
+                <option value="Friday">Friday</option>
+            </select>
+            <label for="side1">Side One:</label>
+            <select name="side1">
+                <option value="0">Select Day</option>
+                <option value="Monday">Monday</option>
+                <option value="Tuesday">Tuesday</option>
+                <option value="Wednesday">Wednesday</option>
+                <option value="Thursday">Thursday</option>
+                <option value="Friday">Friday</option>
+            </select>
+            <label for="dayof">Side One:</label>
+            <select id="day" name="day">
+                <option value="0">Select Day</option>
+                <option value="Monday">Monday</option>
+                <option value="Tuesday">Tuesday</option>
+                <option value="Wednesday">Wednesday</option>
+                <option value="Thursday">Thursday</option>
+                <option value="Friday">Friday</option>
+            </select>
+
+            <input id="submit">
+        </form>
+    </div>
+</div>
+</div>
+<div id="footer">
+    Team Flash
+</div>
+</body>
+</html>
