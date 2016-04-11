0bf92d2d805c0aad46a75e20b7adfd58968de3f1
NeedsMoreGravitas <dyshallow@gmail.com>
2016-04-10 23:16:38 -0500

    able to input menus

--- php/createMenu.php
+++ php/createMenu.php
@@ -1,47 +1,123 @@
-<?php ?>
+<base href="/flash/php/manageItems.php"/> <!-- hax -->
+<?php 
+include_once 'pageNav.php';
+
+
+if(isset($_POST['date'])){
+    insertMenu();
+    outputSuccess();
+    unset($_POST);
+    $_POST = array();
+}
+function outputSuccess(){
+            echo("<p style=\"text-align:center;\">Menu successfully input!</p>");
+        }
+        
+
+function insertMenu(){
+    
+    
+     try {
+        $pdo = getPDO('flash');
+
+        $date = $_POST['date'];
+        $entree = $_POST['entree'];
+        $side1 = $_POST['side1'];
+        $side2 = $_POST['side2'];
+        $special = $_POST['special'];
+        
+
+        $sql = "INSERT INTO `menu` (`menuDate`, `menuEntree`, `menuSide1`, `menuSide2`, `menuSpecial`) VALUES ('$date', '$entree', '$side1', '$side2', '$special')";
+
+        $queryResult = $pdo->query($sql);
+
+    } catch (PDOException $e) {
+        
+    }
+    
+    
+}
+
+
+function populateOptions($type) {
+    try {
+        $pdo = getPDO('flash');
+
+        $sql = "SELECT `itemName`, `itemPrice`, `itemCal` FROM `items` WHERE itemType = '$type'";
+
+        $queryResult = $pdo->query($sql);
+
+
+
+
+        while ($row = $queryResult->fetch(PDO::FETCH_ASSOC)) {
+
+            echo("
+                      <option value=\"{$row['itemName']}\">{$row['itemName']}</option>
+                          ");
+        }
+    } catch (PDOException $e) {
+        
+    }
+}
+
+function getPDO($dbname) {
+    include('../db/flashDB.php');
+
+    try {
+        $pdo = new PDO(DB_CONNECTION_STRING . ";dbname=$dbname", DB_USER, DB_PWD);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        return $pdo;
+    } catch (PDOException $e) {
+        //$GLOBALS['ConfirmationMessage'] = $e->getMessage();
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+
+    <head>
+
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="../css/flash.css">
+        <title>Flash Food Court</title>
+    </head>
+    <body>
+
 <div id="addMenu">
     <div id="form" class="center">
         Select items for the Menu<br><br>
         <form method ="post" action="createMenu.php">
-            <label for="entree">Entree</label>
-            <select name="entree">
-                <option value="0">Select Day</option>
-                <option value="Monday">Monday</option>
-                <option value="Tuesday">Tuesday</option>
-                <option value="Wednesday">Wednesday</option>
-                <option value="Thursday">Thursday</option>
-                <option value="Friday">Friday</option>
-            </select>
+            <label for="date">Date:</label>
+            <input type="date" name="date" value="" required> <br><br>
+            
+            
+            <label for="entree">Entree:</label>
+            <select name="entree" required>
+                <option value=""></option>
+                <?php populateOptions("entree"); ?>
+            </select> <br><br>
             <label for="side1">Side One:</label>
-            <select name="side1">
-                <option value="0">Select Day</option>
-                <option value="Monday">Monday</option>
-                <option value="Tuesday">Tuesday</option>
-                <option value="Wednesday">Wednesday</option>
-                <option value="Thursday">Thursday</option>
-                <option value="Friday">Friday</option>
-            </select>
-            <label for="side1">Side One:</label>
-            <select name="side1">
-                <option value="0">Select Day</option>
-                <option value="Monday">Monday</option>
-                <option value="Tuesday">Tuesday</option>
-                <option value="Wednesday">Wednesday</option>
-                <option value="Thursday">Thursday</option>
-                <option value="Friday">Friday</option>
-            </select>
-            <label for="dayof">Side One:</label>
-            <select id="day" name="day">
-                <option value="0">Select Day</option>
-                <option value="Monday">Monday</option>
-                <option value="Tuesday">Tuesday</option>
-                <option value="Wednesday">Wednesday</option>
-                <option value="Thursday">Thursday</option>
-                <option value="Friday">Friday</option>
-            </select>
+            <select name="side1" required>
+                <option></option>
+                <?php populateOptions("side"); ?>
+            </select><br><br>
+            <label for="side2">Side Two:</label>
+            <select name="side2" required>
+                <option></option>
+                <?php populateOptions("side"); ?>
+            </select><br><br>
+            <label for="special">Special:</label>
+            <select name="special" required>
+                <option></option>
+                <?php populateOptions("special"); ?>
+            </select><br><br>
 
-            <input id="submit">
+            <input type="submit">
         </form>
+        
     </div>
 </div>
 </div>
